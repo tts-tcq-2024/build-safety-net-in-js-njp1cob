030b08e4afbbc3aa1ea42aee1cb057d739359dfe
njp1cob <p.anjana@in.bosch.com>
2024-07-04 20:09:32 +0530

    Update soundex.test.js

--- test/soundex.test.js
+++ test/soundex.test.js
@@ -1,6 +1,6 @@
 const { expect } = require('chai');
 const { generateSoundex } = require('../src/soundex');
-const { soundexFilter } = require('../test/soundex');
+const { soundexFilter } = require('../test/soundex.test.js');
 
 describe('Soundex Algorithm', () => {
     it('should handle empty strings', () => {
@@ -10,6 +10,7 @@ describe('Soundex Algorithm', () => {
     it('should handle single characters', () => {
         expect(generateSoundex("A")).to.equal("A000");
     });
+    
     it('should validate sundex', () => {
         expect(soundexFilter("B", "B", 1 )).to.equal("B000");
     });
