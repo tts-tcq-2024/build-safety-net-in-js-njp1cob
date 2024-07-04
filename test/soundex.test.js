ec959a28b1567f4665358cac5f5b0da6ed0ab68d
njp1cob <p.anjana@in.bosch.com>
2024-07-04 20:36:06 +0530

    Update soundex.test.js

--- test/soundex.test.js
+++ test/soundex.test.js
@@ -17,18 +17,10 @@ describe('Soundex Algorithm', () => {
     it('should handle single characters', () => {
         expect(generateSoundex("A")).to.equal("A000");
     });
-    
-     it('should handle empty input correctly', () => {
-        const name = '';
-        soundexFilter(name, soundex, '');
-
-        
-        expect(soundex).toEqual([]);
-    });
 
 
     it('should validate function', () => {
-        expect(validate()).toHaveBeenCalled();
+        expect(soundexFilter()).toHaveBeenCalled();
     });
     
 });
